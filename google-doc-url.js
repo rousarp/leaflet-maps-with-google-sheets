084b49e4e0dd3f18519df0323b929c0f7a07c076
Pavel Roušar <rousar.pavel@gmail.com>
2020-03-28 00:04:07 +0100

    Update google-doc-url.js
    mofikace zdrojové tabulky

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1iDbuYMOWkPCOnRdLcaaupoH47u5InBQWvEYZ5V-6o9w/edit#gid=0';
